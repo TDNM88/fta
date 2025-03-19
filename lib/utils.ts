b9c01a36d8a46ed1cd3630e6c46ee2c0cb951fde
TDNM88 <167914317+TDNM88@users.noreply.github.com>
2025-03-19 18:08:46 +0700

    Add files via upload

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,29 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { createLogger, format, transports } from 'winston';
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.json()
+  ),
+  defaultMeta: { service: 'fta-app' },
+  transports: [
+    new transports.Console({
+      format: format.combine(
+        format.colorize(),
+        format.simple()
+      )
+    })
+  ]
+});
+
+export default logger;
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
